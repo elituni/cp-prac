3a0a431d8567c2b41e8d33125e0220ff96f757ac
elituni <me@brian.wang>
2024-12-23 17:28:08 -0600

    updated organization

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cp-prac
+
+personal practice for competitive programming like icpc and codeforces.
+
+problem solutions and especially algorithm implementations are not heavily tested and may not be correct.
+
+thanks for visiting!
---
+++ misc/test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    vector<int> test(1);
+    cout << test[3];
+    cout << test.size();
+    cout << test[test.size()+1];
+}
